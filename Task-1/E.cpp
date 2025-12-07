bdc6fbfe5e29ffa19a4b6021c0fc4076868c7400
BasmalaHesham2 <basmalahesham666@gmail.com>
2025-12-07 22:23:04 +0200

    E

--- Task-1/E.cpp
+++ Task-1/E.cpp
@@ -1,41 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-#define vi vector<int>
-#define pii pair<int,int>
-#define all(v) v.begin(), v.end()
+#define int long long 
+#define vi     vector<int>
+#define pii    pair<int, int>
+#define all(v)    v.begin(),v.end()
 #define endl "\n"
-
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v){for (auto& i : v) os << i << ' ';return os;}
+template<typename T> istream& operator>>(istream& is, vector<T>& v){for (auto& i : v) is >> i;return is;}
 void fastIO(){
     ios_base::sync_with_stdio(false);
+
     cin.tie(0);
 }
-
-int32_t main() {
+int32_t main(){
     fastIO();
-    int n, x;
-    cin>>n>>x;
-    vector<pii> a(n); 
-    for(int i=0;i<n;i++) {
-        cin>>a[i].first;
-        a[i].second=i+1;
-    }
-
-    sort(all(a));
-    int i=0,j=n-1;
-
-    while(i<j) {
-        int sum =a[i].first+a[j].first;
-        if (sum==x) {
-            cout<<a[i].second<<" "<<a[j].second;
+    int n,x;cin>>n>>x;
+    vi v(n);
+    map<int,int> mp;
+    for(int i=0;i<n;i++){
+        int num;cin>>num;
+        if(mp.count(x-num)){
+            cout<<mp[x-num]<<" "<<i+1<<endl;
             return 0;
-        } else if(sum<x) {
-            i++;
-        } else {
-            j--;
         }
+        mp[num] = i+1;
     }
 
-    cout << "IMPOSSIBLE";
+    cout<<"IMPOSSIBLE\n";
+
     return 0;
+    
 }
